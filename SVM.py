b5ce148df8ad6814df694481de7bd5356f61fcad
nanomishka <mihail777str@yandex.ru>
2018-11-01 16:54:48 +0300

    add model

--- SVM.py
+++ SVM.py
@@ -80,8 +80,8 @@ class SVMHelper:
         loss_before = loss_fun(X, y, cur_weights)
 
         while True:
-            # happy_indexes = np.random.randint(0, X.shape[0], 100)
-            happy_indexes = range(X.shape[0])
+            happy_indexes = np.random.randint(0, X.shape[0], 1000)
+            # happy_indexes = range(X.shape[0])
 
             X_mod = X[happy_indexes]
             y_mod = y[happy_indexes]
